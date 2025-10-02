af85d71e74fbf06332ec0be203b13937bcc2bf82
Teo Calvo <teocalvo2@gmail.com>
2025-10-02 11:54:13 -0300

    feat: primeiras features

--- src/analytics/report_ciclo_vida.sql
+++ src/analytics/report_ciclo_vida.sql
@@ -1,11 +1,11 @@
 SELECT dtRef,
        descLifeCycle,
-       cluster,
        count(*) AS qtdeCliente
 
 FROM life_cycle
 
 WHERE descLifeCycle <> '05-ZUMBI'
+AND dtRef = (SELECT MAX(dtRef) FROM life_cycle)
 
-group by dtRef, descLifeCycle,cluster
-order by dtRef, descLifeCycle,cluster
+group by dtRef, descLifeCycle
+order by dtRef, descLifeCycle
